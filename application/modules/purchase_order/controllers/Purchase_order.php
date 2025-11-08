3811ab2dfcc7c23b4849ce1f296c8b6596e80746
kmalob <km.alob@academy.opswerks.com>
2025-11-09 04:53:36 +0800

    Move Purchase Order

---
+++ application/modules/purchase_order/controllers/Purchase_order.php
@@ -0,0 +1,82 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Purchase_order extends MY_Controller
+{
+	private $data = [];
+	protected $session;
+	public function __construct()
+	{
+		parent::__construct();
+		$this->session = (object)get_userdata(USER);
+
+		// if(is_empty_object($this->session)){
+		// 	redirect(base_url().'login/authentication', 'refresh');
+		// }
+
+		$model_list = [
+			'purchase_order/Purchase_order_model' => 'poModel',
+			'Item_profiling/Item_profiling_model' => 'ipModel',
+			'inventory/Inventory_model' => 'iModel',
+		];
+		$this->load->model($model_list);
+	}
+
+	public function index(){
+		$this->data['PO_num'] = $this->iModel->get_last_po();
+		$this->data['units'] = $this->ipModel->get_units();
+		$this->data['items_profiles'] = $this->iModel->get_item_profiles();
+		$this->data['supplier'] = $this->iModel->get_suppliers();
+		$this->data['content'] = 'index';
+		$this->load->view('layout', $this->data);
+	}
+
+	public function load_po(){
+		$po_num = $this->input->get("pon");
+		$this->iModel->po = $po_num;
+		// $po_num = "000000";
+		$this->data['po'] = $this->iModel->get_po_header($po_num);
+		$this->data['supplier_deets'] = $this->iModel->get_supplier_data($this->data['po']->supplier_ID);
+		$this->data['po_items'] = $this->iModel->get_po_items($po_num);
+		$this->data['content'] = 'purchase_order';
+		$this->load->view('layout', $this->data);
+	}
+
+	public function load_po_list(){
+		$this->data['purchase_order'] = $this->iModel->get_po_list();
+		$this->data['content'] = 'grid/load_po_list';
+		$this->load->view('layout', $this->data);
+	}
+
+	public function get_po_details()
+    {
+		$po_number = $this->input->get('pon');
+        if (!$po_number) {
+            show_error('PO number is required', 400);
+        }
+		$header = $this->iModel->get_po_header($po_number);
+		$items  = $this->iModel->get_po_items($po_number);
+	
+		$result = [
+			'header' => $header,
+			'items'  => $items
+		];
+
+        if ($result) {
+            $this->output
+                ->set_content_type('application/json')
+                ->set_output(json_encode($result));
+        } else {
+            $this->output
+                ->set_content_type('application/json')
+                ->set_output(json_encode([]));
+        }
+    }
+
+	public function fill_in_item () {
+		$item_ID = $this->input->post('item_ID');
+		$response = $this->iModel->get_item_data($item_ID);
+		echo json_encode($response);
+	}
+
+}
